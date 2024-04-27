1c781662a2d4ef08bcf1adf772d2d3cdac67260b
choijinki83 <168258828+choijinki83@users.noreply.github.com>
2024-04-27 16:07:00 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+"""
+At the command line, only need to run once to install the package via pip:
+
+$ pip install google-generativeai
+"""
+
+from pathlib import Path
+import hashlib
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBJ3BobbZhkYcZeIekKoiuIy-P1W6ewqrg")
+
+# Set up the model
+generation_config = {
+  "temperature": 0.4,
+  "top_p": 1,
+  "top_k": 32,
+  "max_output_tokens": 4096,
+}
+
+safety_settings = [
+  {
+    "category": "HARM_CATEGORY_HARASSMENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_HATE_SPEECH",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+  {
+    "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+    "threshold": "BLOCK_MEDIUM_AND_ABOVE"
+  },
+]
+
+model = genai.GenerativeModel(model_name="gemini-1.0-pro-vision-latest",
+                              generation_config=generation_config,
+                              safety_settings=safety_settings)
+
+uploaded_files = []
+def upload_if_needed(pathname: str) -> list[str]:
+  path = Path(pathname)
+  hash_id = hashlib.sha256(path.read_bytes()).hexdigest()
+  try:
+    existing_file = genai.get_file(name=hash_id)
+    return [existing_file]
+  except:
+    pass
+  uploaded_files.append(genai.upload_file(path=path, display_name=hash_id))
+  return [uploaded_files[-1]]
+
+prompt_parts = [
+  "이밎의 물건을 다양한 용도를 제시해줘. 가장 기본적인 용도부터, 정말 이색적인 사용처도 제시해 줘. 한글로 답변해줘.",
+  *upload_if_needed("<path>/image0.jpeg"),
+]
+
+response = model.generate_content(prompt_parts)
+print(response.text)
+for uploaded_file in uploaded_files:
+  genai.delete_file(name=uploaded_file.name)
